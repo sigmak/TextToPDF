83c65d34862c77172b5eb5e0ebd818b186016698
sigmak <dwfree74@gmail.com>
2020-12-04 16:44:47 +0900

    V-1.1.0
    한글 사용을 위해
    라이브러리 파일을  PDFSharp에서 iTextSharp으로 변경함

--- TextToPdf/Form1.cs
+++ TextToPdf/Form1.cs
@@ -1,5 +1,5 @@
-﻿using PdfSharp.Drawing;
-using PdfSharp.Pdf;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -49,56 +49,69 @@ namespace TextToPdf
             string pdfFileName = "";
             string textFilePath = "";
             string textFileName = "";
-            if (label1.Text=="" || label1.Text=="파일없음")
+            if (label1.Text == "" || label1.Text == "파일없음")
             {
                 return; //함수 탈출
             }
 
-			//출처 : https://mainia.tistory.com/2349
+            //출처 : https://mainia.tistory.com/2349
             textFilePath = Path.GetDirectoryName(label1.Text); //경로 추출
             textFileName = Path.GetFileNameWithoutExtension(label1.Text); // 확장자제외한 파일명만
 
-            pdfFileName = textFilePath + "\\"+ textFileName + ".pdf";
-            //label2.Text = pdfFileName;
-            //참고 : http://csharp.net-informations.com/file/txttopdf.htm
-            try
+            pdfFileName = textFilePath + "\\" + textFileName + ".pdf";
+            label2.Text = pdfFileName;
+
+
+            //출처 : https://www.c-sharpcorner.com/blogs/convert-text-document-to-pdf-file1
+            //Read the Data from Input File
+            //StreamReader rdr = new StreamReader(label1.Text);
+            System.IO.TextReader readFile = new StreamReader(label1.Text);
+
+            //Create a New instance on Document Class
+            Document doc = new Document();
+
+            //Create a New instance of PDFWriter Class for Output File
+            PdfWriter.GetInstance(doc, new FileStream(pdfFileName, FileMode.Create));
+
+            //Open the Document
+            doc.Open();
+
+            // 사용할 폰트의 경로를 가져옵니다. 여기서는 굴림체를 가져왔어요 ㅎ
+            string GulimFont = Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\..\Fonts\gulim.ttc";
+
+            // 폰트의 경로를 iTextSharp의 FontFactory에 등록합니다.
+            FontFactory.Register(GulimFont);
+
+            // iTextSharp의 Font 객체를 생성합니다. 생성할 때에 폰트의 크기를 지정합니다.
+            iTextSharp.text.Font HeaderFont = FontFactory.GetFont("바탕체", BaseFont.IDENTITY_H, 16);
+
+            //System.Drawing.Font textFont = new System.Drawing.Font(baseFont, DEFAULT_FONTSIZE, Font.NORMAL, BaseColor.BLACK);
+            //System.Drawing.Font headingFont = new System.Drawing.Font(baseFont, DEFAULT_HEADINGSIZE, Font.BOLD, BaseColor.BLACK);
+
+            string line = null;
+            while (true)
             {
-                string line = null;
-                System.IO.TextReader readFile = new StreamReader(label1.Text);
-                int yPoint = 0;
-
-                PdfDocument pdf = new PdfDocument();
-                pdf.Info.Title = textFileName; // "TXT to PDF";
-                PdfPage pdfPage = pdf.AddPage();
-                XGraphics graph = XGraphics.FromPdfPage(pdfPage);
-                
-                string GulimFont = Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\..\Fonts\gulim.ttc";
-
-                XFont font = new XFont(GulimFont, 20, XFontStyle.Regular);
-                while(true)
+                line = readFile.ReadLine();
+                if (line == null)
                 {
-                    line = readFile.ReadLine();
-                    if (line == null)
-                    {
-                        break;//  TODO: might not be correct. Was : Exit While
-                    } else
-                    {
-                        graph.DrawString(line,
-                            font,
-                            XBrushes.Black,
-                            new XRect(40, yPoint, pdfPage.Width.Point, pdfPage.Height.Point),
-                            XStringFormats.TopLeft);
-                        yPoint = yPoint + 40;
-                    }
+                    break;//  TODO: might not be correct. Was : Exit While
                 }
-                pdf.Save(pdfFileName);
-                readFile.Close();
-                readFile = null;
-                Process.Start(pdfFileName);
-            } catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
+                else
+                {
+                    Paragraph para = new Paragraph(line + "\n", FontFactory.GetFont("굴림체", BaseFont.IDENTITY_H, 14));
+                    doc.Add(para);
+                }
             }
+
+            //Close the Document
+            doc.Close();
+
+            MessageBox.Show("Conversion Successful...");
+
+            //Open the Converted PDF File
+            System.Diagnostics.Process.Start(pdfFileName);
+
+
 
         }
     }
